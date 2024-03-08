01346868f6112225751ded97d719af9b93a17f78
KangSungLee <rkdtjd1020@naver.com>
2024-03-08 10:04:40 +0900

    Day 2

---
+++ src/main/java/com/example/abbs/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.abbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@GetMapping("/register")
+	public String registerForm() {
+		return "user/register";
+	}
+	
+	@PostMapping("/register")
+	public String registerProc(){
+		return "redirect:/user/login";
+	}
+	
+	@GetMapping("/login")
+	public String loginForm() {
+		return "user/login";
+	}
+	
+	@PostMapping("/login")
+	public String loginProc(){
+		return "redirect:/user/list";
+	}
+}
